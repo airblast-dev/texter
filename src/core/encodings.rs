dd83968c010bc8a50184e8e5170b94bed7e0cd6b
Tayfun Bocek <tayfunbocek@live.ca>
2024-11-11 18:36:53 +0300

    cleanup from encoding change

--- src/core/encodings.rs
+++ src/core/encodings.rs
@@ -1,31 +1,19 @@
 pub(crate) type EncodingFn = fn(&str, usize) -> usize;
 
-pub(crate) const UTF8: fn(&str, usize) -> usize = utf8::exclusive;
+pub(crate) const UTF8: EncodingFn = utf8::exclusive;
 
-pub(crate) const UTF16: fn(&str, usize) -> usize = utf16::exclusive;
+pub(crate) const UTF16: EncodingFn = utf16::exclusive;
 
-pub(crate) const UTF32: fn(&str, usize) -> usize = utf32::exclusive;
+pub(crate) const UTF32: EncodingFn = utf32::exclusive;
 
 pub mod utf8 {
 
-    use super::{between_code_points, char_oob, char_oob_ex};
-
-    /// Finds the byte index for the
-    #[inline]
-    pub(super) fn inclusive(s: &str, nth: usize) -> usize {
-        if s.len() < nth {
-            char_oob(s.len(), nth);
-        };
-        if !s.is_char_boundary(nth) {
-            between_code_points();
-        }
-        nth
-    }
+    use super::{between_code_points, char_oob};
 
     #[inline]
     pub(super) fn exclusive(s: &str, nth: usize) -> usize {
         if s.len() < nth {
-            char_oob_ex(s.len(), nth);
+            char_oob(s.len(), nth);
         };
         if !s.is_char_boundary(nth) {
             between_code_points();
@@ -35,32 +23,7 @@ pub mod utf8 {
 }
 
 pub mod utf16 {
-    use super::between_code_points;
-    use super::char_oob;
-
-    use super::char_oob_ex;
-
-    /// Converts UTF16 indexes to UTF8 indexes.
-    pub(super) fn inclusive(s: &str, nth: usize) -> usize {
-        let mut total_code_points = 0;
-        if nth == 0 {
-            return 0;
-        }
-        for (utf8_index, utf8_len, utf16_len) in s
-            .char_indices()
-            .map(|(i, c)| (i, c.len_utf8(), c.len_utf16()))
-        {
-            if total_code_points > nth {
-                between_code_points();
-            }
-            total_code_points += utf16_len;
-            if total_code_points == nth {
-                return utf8_index + utf8_len;
-            }
-        }
-
-        char_oob(total_code_points, nth)
-    }
+    use super::{between_code_points, char_oob};
 
     /// Converts UTF16 indexes to UTF8 indexes but also allows code point + 1 to be used in range operations.
     pub(super) fn exclusive(s: &str, nth: usize) -> usize {
@@ -85,22 +48,12 @@ pub mod utf16 {
             return nth;
         }
 
-        char_oob_ex(total_code_points, nth)
+        char_oob(total_code_points, nth)
     }
 }
 
 mod utf32 {
-    use super::{char_oob, char_oob_ex};
-
-    #[inline]
-    pub(super) fn inclusive(s: &str, nth: usize) -> usize {
-        let mut counter = 0;
-        let Some((i, _)) = s.char_indices().inspect(|_| counter += 1).nth(nth) else {
-            char_oob(counter, nth);
-        };
-
-        i
-    }
+    use super::char_oob;
 
     #[inline]
     pub(super) fn exclusive(s: &str, nth: usize) -> usize {
@@ -109,7 +62,7 @@ mod utf32 {
             if counter + 1 == nth {
                 return s.len();
             }
-            char_oob_ex(counter, nth);
+            char_oob(counter, nth);
         };
 
         i
@@ -120,13 +73,6 @@ mod utf32 {
 #[inline(never)]
 #[track_caller]
 fn char_oob(byte_index: usize, byte_count: usize) -> ! {
-    panic!("byte index should never more than byte count -> {byte_index} <= {byte_count}")
-}
-
-#[cold]
-#[inline(never)]
-#[track_caller]
-fn char_oob_ex(byte_index: usize, byte_count: usize) -> ! {
     panic!(
         "exclusive byte index should never more than byte count + 1 -> {byte_index} <= {byte_count} + 1"
     )
