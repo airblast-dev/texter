4951b1039515ae2278b84ddeb65789b9d10197df
Tayfun Bocek <tayfunbocek@live.ca>
2024-11-19 18:44:30 +0300

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 pub mod change;
-mod utils;
 mod updateables;
+mod utils;
 
 pub mod core;
 
-#[cfg( feature = "lsp-types")]
+#[cfg(feature = "lsp-types")]
 pub use lsp_types;
 
-#[cfg( feature = "tree-sitter")]
+#[cfg(feature = "tree-sitter")]
 pub use tree_sitter;
